46595a563cf56c094f79a9d808aa8d81246811d4
caralinecyy <1835373375@qq.com>
2015-09-01 11:11:11 +0800

    ok

---
+++ github_steak.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d E:\github\php_everyday
+for /l %%a in (9,9,12) do (
+    for /l %%b in (1,1,31) do (
+        echo 2016-%%a-%%b 11:11:11 >>E:\github\php_everyday\1.txt
+        git add .
+        git commit -m "ok" --date "2015-%%a-%%b 11:11:11"
+    )
+)
