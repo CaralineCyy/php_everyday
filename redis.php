2413344b4e77b577be4f0f0fbe4aea5b9fe0d81c
yunyun.cai <1835373375@qq.com>
2016-02-23 20:29:37 +0800

    redis练习

--- redis.php
+++ redis.php
@@ -6,3 +6,30 @@
    echo "Connection to server sucessfully";
          //查看服务是否运行
    echo "Server is running: " . $redis->ping();
+   echo '<br/>';
+   
+   //get/set操作
+   $redis->set('cyy','123');
+   $retval=$redis->get('cyy');
+   echo $retval;
+   echo '<br/>';
+   
+   
+   $num=$redis->dbsize();
+   echo $num;
+   echo '<br/>';
+   
+   //当前服务状态信息
+   $redis_info=$redis->info();
+   echo '<pre>';
+   print_r($redis_info);
+   echo '<br/>';
+   
+   print_r($redis->keys('*'));
+   echo '<br/>';
+   
+   
+   
+   
+   
+    
