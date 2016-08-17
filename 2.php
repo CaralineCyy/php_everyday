b0addd0060a3eed63399a2fa08b5cb623caf14d7
can_pc <1835373375@qq.com>
2016-08-17 18:11:59 +0800

    测试时间(linux下和windows下不一样)

--- 2.php
+++ 2.php
@@ -1,3 +1,12 @@
 <?
-print_r(phpinfo());
+//date_default_timezone_set("Asia/Shanghai");
+echo time();echo '<br/>';
+
+echo date('Y-m-d',time());
+echo '<br/>';
+echo date('Y-m-d',9999999999);echo '<br/>';
+echo 9999999999;
+//echo "aa";
+echo '<br/>';
+echo strtotime("2038-01-01");
 ?>
